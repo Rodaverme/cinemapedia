bc4a27c72fb057f23187e36c3e64d68f7365fcf0
Ronald Vera <Tu correo>
2024-07-23 21:51:14 -0500

    go router ofical implemented

---
+++ lib/presentation/views/home_views/favorites_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FavoriteView extends StatelessWidget {
+  const FavoriteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite View'),
+        
+      ),
+      body: const Center(child: Text('Favoritos'),),
+    );
+  }
+}
---
+++ lib/presentation/views/home_views/views.dart
@@ -0,0 +1,2 @@
+export 'package:cinemapedia/presentation/views/home_views/home_views.dart';
+export 'favorites_view.dart';
