bc059fd47175c6fa20ddcb4264a8acf66cd59ec2
Rodaverme <ronald.vera@correounivalle.edu.co>
2025-05-06 10:31:35 -0500

    first comit

---
+++ README.md
@@ -0,0 +1,8 @@
+# cinemapedia
+
+# DEV
+1. Copiar el .env.template y renombrarlo a .env
+2. Cambiar las variables de entorno (The MovieDB)
+
+
+
---
+++ lib/infrastructure/datasource/moviedb_datasource.dart
@@ -0,0 +1,34 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+import '../../domain/datasources/movies_datasource.dart';
+
+class MoviedbDatasource extends MovieDatasource {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.theMovieDbKey,
+        'language': 'es-MX',
+        'page': 1,
+      },
+    ),
+  );
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+
+    final moviedbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies =
+        moviedbResponse.results
+            .where((moviedb) => moviedb.posterPath != 'no-poster')
+            .map((moviedb) => MovieMapper.movieDBtoEntity(moviedb))
+            .toList();
+
+    return movies;
+  }
+}
